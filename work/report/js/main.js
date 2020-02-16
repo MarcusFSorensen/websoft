35083be0d7670a67479598ca7051c7ba52637f2a
Marcus Sørensen <marcus.f.s@hotmail.com>
2020-02-16 17:47:08 +0100

    added fetch to main.js in report

--- work/report/js/main.js
+++ work/report/js/main.js
@@ -4,6 +4,14 @@
 (function () {
     "use strict";
 
+    fetch('https://api.scb.se/UF0109/v2/skolenhetsregister/sv/kommun/1081')
+        .then((response) => {
+            return response.json();
+        })
+        .then((myJson) => {
+            console.log(myJson);
+        });
+
 
 
     console.log("All ready.");
