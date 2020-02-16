51b3f7b86bff296ec6469da539d48874b30b1f31
Marcus Sørensen <marcus.f.s@hotmail.com>
2020-02-16 22:50:27 +0100

    h

--- example/javascript/fetch/js/main.js
+++ example/javascript/fetch/js/main.js
@@ -24,8 +24,8 @@
     //     drawFlagElfenbenskusten();
     // });
 
-    fetch('https://api.scb.se/UF0109/v2/skolenhetsregister/sv/kommun/1081', {mode: 'no-cors'})
-    //fetch('data/1081.json')
+    //fetch('https://api.scb.se/UF0109/v2/skolenhetsregister/sv/kommun/1081', {mode: 'no-cors'})
+    fetch('data/1081.json')
         .then((response) => {
             return response.json();
         })
