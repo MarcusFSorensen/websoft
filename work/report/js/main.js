c1b11389f81a4c4361d090e11fb1911eeee8c2d8
Marcus Sørensen <marcus.f.s@hotmail.com>
2020-02-17 13:34:48 +0100

    Cleaning code

--- work/report/js/main.js
+++ work/report/js/main.js
@@ -3,31 +3,35 @@
  */
 (function () {
     "use strict";
+
     console.log("All ready.");
+
+    const tablebody = document.getElementById('schools');
+
+    var fetchSchools = function(){
+        fetch('Data/Kristianstad.json')
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            showSchools(data.Skolenheter);
+        });
+    }
+    
+    var showSchools = function(data){
+        //var table = document.getElementById('schools');
+        tablebody.innerHTML = "";
+        data.forEach(object =>{
+            var tr = document.createElement('tr');
+            tr.innerHTML = '<td>' + object.Skolenhetskod + '</td>' +
+            '<td>' + object.Skolenhetsnamn + '</td>' +
+            '<td>' + object.Kommunkod + '</td>' +
+            '<td>' + object.PeOrgNr + '</td>';
+            table.appendChild(tr);
+        })
+    }
 }());
 
-var kristianstadSchools;
 
-var fetchSchools = function(){
-    fetch('Data/Kristianstad.json')
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        console.log(data);
-        showSchools(data.Skolenheter);
-    });
-}
-
-var showSchools = function(data){
-    var table = document.getElementById('schools');
-    data.forEach(object =>{
-        var tr = document.createElement('tr');
-        tr.innerHTML = '<td>' + object.Skolenhetskod + '</td>' +
-        '<td>' + object.Skolenhetsnamn + '</td>' +
-        '<td>' + object.Kommunkod + '</td>' +
-        '<td>' + object.PeOrgNr + '</td>';
-        table.appendChild(tr);
-    })
-}
 
