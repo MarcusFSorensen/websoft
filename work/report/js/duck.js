1a5fc93d287f136d590e21518ce6795567a346a3
Marcus Sørensen <marcus.f.s@hotmail.com>
2020-02-17 15:14:54 +0100

    added a duck to my pages

---
+++ work/report/js/duck.js
@@ -0,0 +1,27 @@
+(function () {
+    'use strict';
+
+    var area = document.body,
+        areaHeight = window.innerHeight,
+        areaWidth = window.innerWidth,
+        duck = document.createElement('img');
+        
+    duck.src='img/duck.png';
+    duck.style.position ='absolute';
+    duck.style.left = '0px';
+    duck.style.top = '0px';
+    duck.style.height = '50px';
+    duck.style.width = '50px';
+    duck.style.zIndex = 10000;
+    duck.addEventListener('click', respawnDuck);
+
+    function respawnDuck(){
+        var newX = Math.floow(Math.random() * (areaWidth-duck.width)),
+        newY = Math.floor(Math.random() * (areaHeight-duck.height));
+    
+        duck.style.left = newX+'px';
+        duck.style.top = newY+'px';
+        area.appendChild(duck);
+    }
+
+})();
