ee1bec011b76d70d7f725b6a9f3adf1ad8b732bf
Marcus Sørensen <marcus.f.s@hotmail.com>
2020-02-17 09:11:10 +0100

    added function to fill tables

--- work/report/js/main.js
+++ work/report/js/main.js
@@ -9,14 +9,27 @@
 
 var kristianstadSchools;
 
-var showSchools = function(){
+var fetchSchools = function(){
     fetch('Data/Kristianstad.json')
     .then((response) => {
         return response.json();
     })
     .then((myJson) => {
         kristianstadSchools = myJson;
+        showSchools(kristianstadSchools);
         console.log(kristianstadSchools);
     });
+}
+
+var showSchools = function(data){
+    var table = document.getElementById('schools');
+    data.forEach(function(object){
+        var tr = document.createElement('tr');
+        tr.innerHTML = '<td>' + object.Skolenhetskod + '</td>' +
+        '<td>' + object.Skolenhetsnamn + '</td>' +
+        '<td>' + object.Kommunkod + '</td>' +
+        '<td>' + object.PeOrgNr + '</td>';
+        table.appendChild(tr);
+    })
 }
 
